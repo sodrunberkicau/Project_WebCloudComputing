5a89235dfa4406b2c78e53b9dd7b377c4af8fece
sodrunberkicau <sodrunberkicau@gmail.com>
2025-04-10 10:21:16 +0700

    progress 100%, make ui in dashboard main

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { DashboardLayout } from "@/components/dashboard-layout"
+import { DashboardPage } from "@/components/dashboard-page"
 
 export default function Home() {
   return (
     <DashboardLayout>
-      Welcome
+      <DashboardPage/>
     </DashboardLayout>
   )
 }
